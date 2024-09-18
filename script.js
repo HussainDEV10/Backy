ca1539391197f528542e9f89b71855a8b4e11222
HussainDEV10 <trendshussain@gmail.com>
2024-09-18 15:17:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // إعداد Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBXXCR2jN8SOP_AamRaE0vkEliR_cnpLqY",
-    authDomain: "backy-123.firebaseapp.com",
-    projectId: "backy-123",
-    storageBucket: "backy-123.appspot.com",
-    messagingSenderId: "763792380953",
-    appId: "1:763792380953:web:74e509e70ca36b94f80688",
-    measurementId: "G-YP8852THBW"
+  apiKey: "AIzaSyBXXCR2jN8SOP_AamRaE0vkEliR_cnpLqY",
+  authDomain: "backy-123.firebaseapp.com",
+  projectId: "backy-123",
+  storageBucket: "backy-123.appspot.com",
+  messagingSenderId: "763792380953",
+  appId: "1:763792380953:web:74e509e70ca36b94f80688",
+  measurementId: "G-YP8852THBW"
 };
+
 
 // تهيئة Firebase
 const app = firebase.initializeApp(firebaseConfig);
